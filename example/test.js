5f5071ea801205bf1c31cebc326eee50f581cf52
mmis1000 <mmis10002@gmail.com>
2019-12-27 04:11:50 +0000

    Add more test

--- example/test.js
+++ example/test.js
@@ -4,5 +4,15 @@ import { createRealm } from '../lib/browserRealm.js'
 
 var s = createRealm()
 s.console = console
-s.test = new Int8Array(10)
-s.eval('debugger; console.log("hi")')
+const test = new Int8Array(10)
+test[0] = 5
+s.test = test
+s.eval(`
+    debugger;
+    console.log("hi");
+    console.log(test[0]);
+    test.__proto__.hello = function () { console.log('first el is ' + this[0]) }
+    test[0] = 6
+    test.hello()
+`)
+console.log(s.test[0], s.test.hello)
